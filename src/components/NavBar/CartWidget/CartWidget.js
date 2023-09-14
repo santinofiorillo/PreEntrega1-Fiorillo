7b81219393e6d179394f610a41f9e4320f5b68b7
santino fiorillo <santinofiorillo62@gmail.com>
2023-09-13 22:37:54 -0300

    Shoes Style, comenzando...

---
+++ src/components/NavBar/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import carrito from './assets/carrito.png' 
+
+const CardWidget = () => {
+    return(
+        <div>
+            <img src= {carrito} alt="cart-widget" />
+            0
+        </div>
+    )
+
+}
+export default CardWidget
