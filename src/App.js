4c0a21afa97e0fc6798520746b5d84e2b852dc6c
santino fiorillo <santinofiorillo62@gmail.com>
2023-10-02 23:11:10 -0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import NavBar from './components/NavBar/NavBar';
 import './App.css';
-import ItemListContainer from './components/NavBar/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting= {"Bienvenidos a mi tienda "}/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+        <Route path='/' element={<ItemListContainer />}/>
+        <Route path='/category/:categoryId' element={<ItemListContainer />}/>
+        <Route path='/item/:itemId' element={<ItemDetailContainer />}/>
+        <Route path='*' element={<h1>404 NOT FOUND </h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,36 +1,20 @@
-import CardWidget from './CartWidget/CartWidget';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import './NavBar.css'
+import CartWidget from '../CartWidget/CartWidget'
+import { NavLink, Link } from 'react-router-dom'
 
 function NavBar() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">Shoes Styles</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Ultimos Ingresos</Nav.Link>
-                        <Nav.Link href="#link">Ofertas</Nav.Link>
-                        <NavDropdown title="Calsados" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Zapatillas</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Botines
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3"></NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-            <CardWidget/>
-        </Navbar>
+        <nav className="NavBar">
+            <Link to='/'>
+                <h3>Ecommerce</h3>
+            </Link>
+            <div className='Categories'>
+                <NavLink to={`/category/celular`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Celulares</NavLink>
+                <NavLink to={`/category/tablet`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Tablets</NavLink>
+                <NavLink to={`/category/notebook`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Notebooks</NavLink>
+            </div>
+            <CartWidget/>
+        </nav>
     )
 }
 export default NavBar
